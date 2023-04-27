77722ab6998ab0529e8c3600660be823e83c7313
David Ihl <daih1985@gmail.com>
2023-04-27 17:46:48 +0200

    Simple Webserver

--- index.js
+++ index.js
@@ -5,18 +5,20 @@ import https from 'node:https';
 // Definitions
 const host = 'localhost';
 const port = 3000;
+const _dirname = '/public';
 
 const ssl = {
   key: fs.readFileSync('server.key'),
   cert: fs.readFileSync('server.cert'),
 };
 
-const requestListener = function (request, response) {
-  response.writeHead(200);
-  response.end('My first webserver!');
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end('My first server!');
 };
 
 const server = http.createServer(requestListener);
+
 server.listen(port, host, () => {
   console.log(`Server is running on http://${host}:${port}`);
 });
