a1538d18226b9248774c5f245641f92474e81fa3
David Ihl <daih1985@gmail.com>
2023-04-29 21:47:08 +0200

    Set correct header based on extname  or return 404

--- index.js
+++ index.js
@@ -8,22 +8,49 @@ const port = 3000;
 const root = 'public';
 
 const requestListener = function (req, res) {
+  if(req.url.slice(-1) === "/") {
+  const filePath = path.join(root, req.url, 'index.htm');
+  } else {
   const filePath = path.join(root, req.url);
+  }
   //  console.log('Accessing ' + filePath + '...');
   //  console.log(req.url);
+  console.log(req.url);
   console.log(filePath);
 
   fs.promises
     .readFile(filePath)
     .then((contents) => {
-      res.setHeader('Content-Type', 'text/html');
+
+      //Log what filetype is called
+      console.log(path.extname(filePath));
+
+      //Return the correct setHeader
+      if(path.extname(filePath) === '.html') {
+        res.setHeader('Content-Type', 'text/html');
+      } else if (path.extname(filePath) === '.htm') {
+        res.setHeader('content-type', 'text/html');
+      } else if (path.extname(filePath) === '.jpg') {
+        res.setHeader('content-type', 'image/jpg');
+      } else if (path.extname(filePath) === '.css') {
+        res.setHeader('content-type', 'text/css');
+      } else {
+        res.setHeader('content-type', 'text/plain');
+      }
+      
       res.writeHead(200);
       res.end(contents);
     })
-    .catch((err) => {
-      res.writeHead(500);
-      res.end(err);
-      return;
+    .catch((err) => {      
+      fs.promises
+        .readFile('404.html')
+        .then((contents) => {
+          res.statusCode = 404;
+          res.setHeader('Content-Type', 'text/html');
+          res.end(contents);
+          console.log(err.message);
+          return;
+        })
     });
 };
 
