b895d4b2216b28d4ef48bb745d8d86f1ac081b42
David Ihl <daih1985@gmail.com>
2023-04-29 23:04:57 +0200

    Fix code for preflight

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import fs from 'fs';
-import http from 'http';
-import path from 'path';
+import fs from 'node:fs';
+import http from 'node:http';
+import path from 'node:path';
 
 // Definitions
 const host = 'localhost';
@@ -8,11 +8,11 @@ const port = 3000;
 const root = 'public';
 
 const requestListener = function (req, res) {
-  let filePath = "";
-  if(req.url.slice(-1) === "/") {
-  filePath = path.join(root, req.url, 'index.html');
+  let filePath = '';
+  if (req.url.slice(-1) === '/') {
+    filePath = path.join(root, req.url, 'index.html');
   } else {
-  filePath = path.join(root, req.url);
+    filePath = path.join(root, req.url);
   }
   //  console.log('Accessing ' + filePath + '...');
   //  console.log(req.url);
@@ -20,17 +20,16 @@ const requestListener = function (req, res) {
   console.log(filePath);
   console.log(path.extname(req.url));
 
-
   fs.promises
     .readFile(filePath)
     .then((contents) => {
       res.statusCode = 200;
 
-      //Log what filetype is called
+      // Log what filetype is called
       console.log(path.extname(filePath));
 
-      //Return the correct setHeader
-      if(path.extname(filePath) === '.html') {
+      // Return the correct setHeader
+      if (path.extname(filePath) === '.html') {
         res.setHeader('Content-Type', 'text/html');
       } else if (path.extname(filePath) === '.htm') {
         res.setHeader('content-type', 'text/html');
@@ -41,30 +40,25 @@ const requestListener = function (req, res) {
       } else {
         res.setHeader('content-type', 'text/plain');
       }
-      
+
       res.writeHead(200);
       res.end(contents);
-
     })
     .catch((err) => {
-      if(path.basename(filePath) === 'index.html'){
-        fs.promises
-          .readFile(filePath.slice(0, -1))
-          .then((contents) => {
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/html');
-            res.end(contents);
-          })
+      if (path.basename(filePath) === 'index.html') {
+        fs.promises.readFile(filePath.slice(0, -1)).then((contents) => {
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'text/html');
+          res.end(contents);
+        });
       } else {
-        fs.promises
-          .readFile('404.html')
-          .then((contents) => {
-            res.statusCode = 404;
-            res.setHeader('Content-Type', 'text/html');
-            res.end(contents);
-            console.log(err.message);
-            return;
-          })
+        fs.promises.readFile('404.html').then((contents) => {
+          res.statusCode = 404;
+          res.setHeader('Content-Type', 'text/html');
+          res.end(contents);
+          console.log(err.message);
+          return;
+        });
       }
     });
 };
